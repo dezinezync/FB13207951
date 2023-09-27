c78a688afe4bb5eb8d4d06402742b5e5a5ca493c
Nikhil Nigade <nikhil@dezinezync.com>
2023-09-27 10:21:21 +0530

    Initial Commit

---
+++ WidgetsDebug/WidgetsDebugApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsDebugApp.swift
+//  WidgetsDebug
+//
+//  Created by Nikhil Nigade on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct WidgetsDebugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
