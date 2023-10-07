c81cb095fb9301c3a6704c404c7931d4a0078017
Nikhil Nigade <nikhil@dezinezync.com>
2023-10-07 08:01:42 +0530

    updated sample app
    Signed-off-by: Nikhil Nigade <nikhil@dezinezync.com>

--- widgets/DebugWidget.swift
+++ widgets/DebugWidget.swift
@@ -15,7 +15,7 @@ struct DebugWidget: Widget {
       kind: "debugWidget",
       provider: DebugProvider(),
       content: { entry in
-        Text("\(entry.date.timeIntervalSinceReferenceDate)")
+        DebugView(date: entry.date)
           .containerBackground(Color("WidgetBackground"), for: .widget)
       })
     .contentMarginsDisabled()
@@ -46,5 +46,56 @@ final class DebugProvider: TimelineProvider {
     let entry = Entry(date: Date())
     let timeline = Timeline(entries: [entry], policy: .never)
     completion(timeline)
+  }
+}
+
+// MARK: - DebugView
+struct DebugView: View {
+  let date: Date
+  
+  var body: some View {
+    Grid {
+      ForEach(0..<8) { row in
+        GridRow {
+          ForEach(0..<3, id: \.self) { column in
+            Link(destination: URL(string: "widgetsdebug://tab/1")!, label: {
+              MyView(date: date.addingTimeInterval(86400 * TimeInterval((row * 2) + column)))
+            })
+          }
+        }
+      }
+    }
+  }
+}
+
+// MARK: - Misc. Views
+struct MyView: View {
+  let date: Date
+  
+  var body: some View {
+    Link(destination: URL(string: "widgetsdebug://tab/2")!, label: {
+      SomeView(date: date)
+    })
+  }
+}
+
+struct SomeView: View {
+  let date: Date
+  
+  var body: some View {
+    Link(destination: URL(string: "widgetsdebug://tab/3")!, label: {
+      LinkedView(date: date)
+    })
+  }
+}
+
+struct LinkedView: View {
+  let date: Date
+  
+  var body: some View {
+    Link(destination: URL(string: "widgetsdebug://tab/4")!, label: {
+      Text("\(Int(date.timeIntervalSinceReferenceDate / 86400))")
+        .font(.footnote)
+    })
   }
 }
